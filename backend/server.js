7b94ce6c8ded29ea857120af40690ce32732ad60
shinytornado <shashankspothula@gmail.com>
2024-06-18 22:25:00 +0530

    temp-changes

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 import 'dotenv/config'
 
 
 //app config
 const app = express()
-const port = 4000
+const port =  process.env.PORT || 4000;
 
 //middleware
 app.use(express.json())
@@ -23,6 +24,7 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/", (req, res) => {
         res.send("API working")
